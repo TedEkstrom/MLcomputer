9d06a31481d4b7144039b69defd05d4348eca78d
Ted Ekström <80516576+TedEkstrom@users.noreply.github.com>
2024-09-19 14:50:10 +0200

    Add files via upload

---
+++ mlcomputer.ps1
@@ -0,0 +1,115 @@
+﻿
+$user = WHOAMI /UPN
+
+while (1) {
+   
+    $input = ""
+
+    while (!$input) {
+        Write-Host "
+
+        current user: $user
+        
+            [1] Check if on
+            [2] Check nvidia-smi
+            [3] Check nvidia-smi in docker
+            [4] Remove all docker images
+            [5] Remove all docker containers
+            [6] Check IP-address
+            [7] Send command
+            [8] Connect with ssh
+            [9] Change user
+            [10] Exit
+        "
+        $input = Read-Host -Prompt "choose? "
+        Write-Host ""
+    }
+    
+    switch ($input) {
+        
+        1 { 1..8 | ForEach-Object { Write-Host "Checking mlcomputer00$_" -ForegroundColor DarkYellow; ; ping mlcomputer00$_ -n 1; Write-Host ""}  }
+
+        2 { $password = Read-Host "Type password " -AsSecureString;
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $password
+            $session = New-SSHSession -ComputerName mlcomputer00$_ -Credential $Credential
+            1..8 | ForEach-Object { 
+                Write-Host "############################################################################################"
+                Write-Host ""
+                Write-Host "Checking nvidia-smi on mlcomputer00$_" -ForegroundColor DarkYellow; 
+                (Invoke-SSHCommand -SSHSession $session -Command nvidia-smi).Output
+            } 
+          }
+
+        3 { $password = Read-Host "Type password " -AsSecureString;
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $password
+            $session = New-SSHSession -ComputerName mlcomputer00$_ -Credential $Credential
+            1..8 | ForEach-Object { 
+                Write-Host "############################################################################################"
+                Write-Host ""
+                Write-Host "Checking nvidia-smi in Container mlcomputer00$_" -ForegroundColor DarkYellow;  
+                (Invoke-SSHCommand -SSHSession $session -Command "docker run --rm --gpus all ubuntu nvidia-smi").Output
+            } 
+          }
+
+        4 { $password = Read-Host "Type password " -AsSecureString;
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $password
+            $session = New-SSHSession -ComputerName mlcomputer00$_ -Credential $Credential
+            1..8 | ForEach-Object { 
+                Write-Host "############################################################################################"
+                Write-Host ""
+                Write-Host "Remove all docker image on mlcomputer00$_" -ForegroundColor DarkYellow;  
+                (Invoke-SSHCommand -SSHSession $session -Command 'docker rmi -f $(docker images -aq)').Output
+            } 
+          }
+
+        5 { $password = Read-Host "Type password " -AsSecureString;
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $password
+            $session = New-SSHSession -ComputerName mlcomputer00$_ -Credential $Credential
+            1..8 | ForEach-Object { 
+                Write-Host "############################################################################################"
+                Write-Host ""
+                Write-Host "Remove all docker container on mlcomputer00$_" -ForegroundColor DarkYellow;  
+                (Invoke-SSHCommand -SSHSession $session -Command 'docker rm -vf $(docker ps -aq)').Output
+            } 
+          }
+
+        6 { $password = Read-Host "Type password " -AsSecureString;
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $password
+            $session = New-SSHSession -ComputerName mlcomputer00$_ -Credential $Credential
+            1..8 | ForEach-Object { 
+                Write-Host "############################################################################################"
+                Write-Host ""
+                Write-Host "Checking IP address for mlcomputer00$_" -ForegroundColor DarkYellow; 
+            } 
+          }
+
+        7 { 
+            $password = Read-Host "Type password " -AsSecureString;
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $password
+            $session = New-SSHSession -ComputerName mlcomputer00$_ -Credential $Credential
+            $command = Read-Host -Prompt "Command to execute "
+            1..8 | ForEach-Object { 
+                Write-Host "############################################################################################"
+                Write-Host ""
+                Write-Host "Send command to mlcomputer00$_" -ForegroundColor DarkYellow; 
+                (Invoke-SSHCommand -SSHSession $session -Command $command ).Output
+            } 
+          }
+
+        8 { 
+          
+            1..8 | ForEach-Object { 
+                Write-Host "############################################################################################"
+                Write-Host ""
+                Write-Host "Connect by SSH to mlcomputer00$_" -ForegroundColor DarkYellow; 
+                ssh $user@mlcomputer00$_
+            } 
+          }
+
+        9 { $user = Read-Host -Prompt "User to use "
+          }
+
+        10 { Exit }
+    }
+    Remove-SSHSession -SSHSession $session
+}
